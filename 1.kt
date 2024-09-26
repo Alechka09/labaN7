4d98e0fa1a8174a67c308cf182c1f4efd4a1d577
Your Name <you@example.com>
2024-09-26 21:07:41 +0900

    3

--- 1.kt
+++ 1.kt
@@ -1,3 +1,27 @@
-
+//Написать консольный калькулятор
 fun main(){
+    println("введите число: " )
+    val firstNumber = readLine()?.toDoubleOrNull()
+    val secondNumber = readLine()?.toDoubleOrNull()
+
+    if(firstNumber!=null && secondNumber !=null){
+        println(" введите операцию + , - , / , * ")
+        val o = readLine().toString()[0]
+        var rez: Double? = null
+
+        when(o) {
+            '+' -> {rez =firstNumber + secondNumber}
+            '-' -> {rez =firstNumber - secondNumber}
+            '/' -> {rez =firstNumber / secondNumber}
+            '*' -> {rez =firstNumber * secondNumber}
+            else -> {
+                println("выберите операцию и введите ее")
+            }
+        }
+        if (rez != null) {
+            println("$firstNumber $o $secondNumber = $rez")}
+    }
 }
+
+
+
